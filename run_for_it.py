4b48c05b4f39bb678706da72b3e6849eb07a7c14
Maria Master <mgomesmaster@gmail.com>
2023-04-03 10:36:25 -0400

    docstrings for experimental sorting function

--- run_for_it.py
+++ run_for_it.py
@@ -13,13 +13,5 @@ def welcome(): #Ashley Kharbanda
         This function will display to users the rules of the game with an 
         example roll
     """
-
-def sorting_sequence(sequence): #Maria Master
-    """ An experimental function for this exercise; 
-        This function will sort the 6 numbers that the player gets after rolling
-        their dices.
-        
-    Args:
-        sequence(list of int): list of numbers corresponding to the 6 rolls.
-    """
     
+#lily
