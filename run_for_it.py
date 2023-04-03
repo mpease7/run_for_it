a0a0ae2a0d057f78f8402754826b60db8d3dcb03
Maria Master <mgomesmaster@gmail.com>
2023-04-03 10:10:37 -0400

    docstrings for experimental sorting function

--- run_for_it.py
+++ run_for_it.py
@@ -1,1 +1,10 @@
 """This is a implementation of the game 'Run For It'"""
+
+def sorting_sequence(sequence): #Maria Master
+    """ An experimental function for this exercise; 
+        This function will sort the 6 numbers that the player gets after rolling
+        their dices.
+        
+    Args:
+        sequence(list of int): list of numbers corresponding to the 6 rolls.
+    """
