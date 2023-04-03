1010b9db4c11c6a92b8865251d7e255a0dfaf09e
Ashley Kharbanda <ashleymk03@gmail.com>
2023-04-03 10:31:40 -0400

    added run_for_it.py

--- run_for_it.py
+++ run_for_it.py
@@ -1,1 +1,15 @@
 """This is a implementation of the game 'Run For It'"""
+
+def sorting_sequence(sequence): #Maria Master
+    """ An experimental function for this exercise; 
+        This function will sort the 6 numbers that the player gets after rolling
+        their dices.
+        
+    Args:
+        sequence(list of int): list of numbers corresponding to the 6 rolls.
+    """
+def welcome(): #Ashley Kharbanda
+    """A experimental function for this exercise;
+        This function will display to users the rules of the game with an 
+        example roll
+    """
