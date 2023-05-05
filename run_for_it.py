0bdc9214cf142c3eebd88e6a46d66c46be6306c2
Maria Master <mgomesmaster@gmail.com>
2023-05-05 10:47:00 -0400

    line break in Argument Parser class

--- run_for_it.py
+++ run_for_it.py
@@ -314,8 +314,10 @@ def parse_args(arglist):
         ArgumentParser class.
     """
     parser = ArgumentParser()
-    parser.add_argument("-p1", "--player1_name",default= "player1", help = "name of player 1")
-    parser.add_argument("-p2", "--player2_name",default= "player2", help = "name of player 2")
+    parser.add_argument("-p1", "--player1_name",default= "player1", 
+                        help = "name of player 1")
+    parser.add_argument("-p2", "--player2_name",default= "player2", 
+                        help = "name of player 2")
     return parser.parse_args(arglist)
 
 if __name__ == "__main__":
